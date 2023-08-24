04d7b9dcff0ae46bf2e6fa07f1da8e11f0c59616
jorge.sanchez.escobedo@gmail.com <jorge.sanchez.escobedo@gmail.com>
2023-08-23 22:36:57 -0600

    calculator

--- internal/app/receipt/calculator/points.go
+++ internal/app/receipt/calculator/points.go
@@ -2,10 +2,132 @@ package calculator
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+	"time"
+	"unicode"
 
 	"receipt-processor-challenge/internal/domain/receipt"
 )
 
+const (
+	datePurchaseFormat string = "2006-01-02"
+	timePurchaseFormat string = "15:04"
+)
+
+var (
+	ErrDate = errors.New("invalid date format")
+	ErrTime = errors.New("invalid time format")
+)
+
+/**
+RULES
+One point for every alphanumeric character in the retailer name.
+50 points if the total is a round dollar amount with no cents.
+25 points if the total is a multiple of 0.25.
+5 points for every two items on the receipt.
+If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer. The result is the number of points earned.
+6 points if the day in the purchase date is odd.
+10 points if the time of purchase is after 2:00pm and before 4:00pm.
+***/
+
 func Points(ctx context.Context, r receipt.Receipt) (*receipt.Points, error) {
-	return nil, nil
+	purchaseDate, err := time.Parse(datePurchaseFormat, r.PurchaseDate)
+	if err != nil {
+		return nil, ErrDate
+	}
+
+	purchaseTime, err := time.Parse(timePurchaseFormat, r.PurchaseTime)
+	if err != nil {
+		return nil, ErrTime
+	}
+
+	points := retailerNamePoints(r.Retailer)
+	points += roundDollarPoints(r.Total)
+	points += multipleOf25CentsPoints(r.Total)
+	points += itemsPoints(r.Items)
+	points += trimmedDescriptionPoints(r.Items)
+	points += oddPurchaseDayPoints(purchaseDate)
+	points += timePurchasePoints(purchaseTime)
+
+	return &receipt.Points{Points: points}, nil
+}
+
+func retailerNamePoints(name string) int {
+	points := 0
+	for _, c := range name {
+		if unicode.IsLetter(c) || unicode.IsNumber(c) {
+			points++
+		}
+	}
+
+	return points
+}
+
+func roundDollarPoints(total float32) int {
+	test := float64(total)
+
+	if test == math.Trunc(test) {
+		return 50
+	}
+
+	return 0
+}
+
+func multipleOf25CentsPoints(total float32) int {
+	const frac float64 = 0.25
+	if math.Mod(float64(total), frac) == 0 {
+		return 25
+	}
+
+	return 0
+}
+
+func itemsPoints(items []receipt.Item) int {
+	return (len(items) / 2) * 5
+}
+
+func trimmedDescriptionPoints(items []receipt.Item) int {
+	total := 0
+
+	for _, item := range items {
+		x := strings.TrimSpace(item.ShortDescription)
+		l := len(x)
+		if l%3 == 0 {
+			test := float64(item.Price * 0.2)
+
+			if test == math.Trunc(test) {
+				total += int(test)
+				continue
+			}
+
+			fl := fmt.Sprintf("%f", test)
+			text := strings.Split(fl, ".")
+			itext, err := strconv.Atoi(text[0])
+			if err == nil {
+				total += itext + 1
+			}
+		}
+
+	}
+
+	return total
+}
+
+func oddPurchaseDayPoints(date time.Time) int {
+	if date.Day()%2 == 0 {
+		return 0
+	}
+	return 6
+}
+
+func timePurchasePoints(t time.Time) int {
+	if t.Hour() >= 14 && t.Hour() <= 16 {
+		return 10
+	}
+
+	return 0
 }
---
+++ internal/app/receipt/calculator/points_test.go
@@ -0,0 +1,400 @@
+package calculator
+
+import (
+	"context"
+	"errors"
+	"receipt-processor-challenge/internal/domain/receipt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_RetailerNamePoints(t *testing.T) {
+	cases := []struct {
+		name           string
+		retailName     string
+		expectedResult int
+	}{
+		{
+			name:           "case-Target",
+			retailName:     "Target",
+			expectedResult: 6,
+		},
+		{
+			name:           "case-M&M Corner Market",
+			retailName:     "M&M Corner Market",
+			expectedResult: 14,
+		},
+	}
+
+	for _, c := range cases {
+		rname := c.retailName
+		expectedPoints := c.expectedResult
+
+		t.Run(c.name, func(t *testing.T) {
+			points := retailerNamePoints(rname)
+
+			assert.Equal(t, expectedPoints, points)
+		})
+	}
+}
+
+func Test_roundDollarPoints(t *testing.T) {
+	cases := []struct {
+		name           string
+		total          float32
+		expectedResult int
+	}{
+		{
+			name:           "total-rounded",
+			total:          9.00,
+			expectedResult: 50,
+		},
+		{
+			name:           "total-no-rounded",
+			total:          9.25,
+			expectedResult: 0,
+		},
+	}
+
+	for _, c := range cases {
+		total := c.total
+		expectedPoints := c.expectedResult
+
+		t.Run(c.name, func(t *testing.T) {
+			points := roundDollarPoints(total)
+
+			assert.Equal(t, expectedPoints, points)
+		})
+	}
+}
+
+func Test_multipleOf25CentsPoints(t *testing.T) {
+	cases := []struct {
+		name           string
+		total          float32
+		expectedResult int
+	}{
+		{
+			name:           "is-multiple",
+			total:          9.0,
+			expectedResult: 25,
+		},
+		{
+			name:           "is-not-multiple",
+			total:          9.1,
+			expectedResult: 0,
+		},
+	}
+
+	for _, c := range cases {
+		total := c.total
+		expectedPoints := c.expectedResult
+
+		t.Run(c.name, func(t *testing.T) {
+			points := multipleOf25CentsPoints(total)
+
+			assert.Equal(t, expectedPoints, points)
+		})
+	}
+}
+
+func Test_itemsPoints(t *testing.T) {
+	cases := []struct {
+		name           string
+		items          []receipt.Item
+		expectedResult int
+	}{
+		{
+			name:           "test-0-tems",
+			items:          []receipt.Item{},
+			expectedResult: 0,
+		},
+		{
+			name: "test-5-tems",
+			items: []receipt.Item{
+				{
+					ShortDescription: "Mountain Dew 12PK",
+					Price:            6.49,
+				}, {
+					ShortDescription: "Emils Cheese Pizza",
+					Price:            12.25,
+				}, {
+					ShortDescription: "Knorr Creamy Chicken",
+					Price:            1.26,
+				}, {
+					ShortDescription: "Doritos Nacho Cheese",
+					Price:            3.35,
+				}, {
+					ShortDescription: "   Klarbrunn 12-PK 12 FL OZ  ",
+					Price:            12.00,
+				},
+			},
+			expectedResult: 10,
+		},
+	}
+
+	for _, c := range cases {
+		items := c.items
+		expectedPoints := c.expectedResult
+
+		t.Run(c.name, func(t *testing.T) {
+			points := itemsPoints(items)
+
+			assert.Equal(t, expectedPoints, points)
+		})
+	}
+}
+
+func Test_trimmedDescriptionPoints(t *testing.T) {
+	cases := []struct {
+		name           string
+		items          []receipt.Item
+		expectedResult int
+	}{
+		{
+			name: "Klarbrunn-case",
+			items: []receipt.Item{
+				{
+					ShortDescription: "Mountain Dew 12PK",
+					Price:            6.49,
+				}, {
+					ShortDescription: "Emils Cheese Pizza",
+					Price:            12.25,
+				}, {
+					ShortDescription: "Knorr Creamy Chicken",
+					Price:            1.26,
+				}, {
+					ShortDescription: "Doritos Nacho Cheese",
+					Price:            3.35,
+				}, {
+					ShortDescription: "   Klarbrunn 12-PK 12 FL OZ  ",
+					Price:            12.00,
+				},
+			},
+			expectedResult: 6,
+		},
+	}
+
+	for _, c := range cases {
+		items := c.items
+		expectedPoints := c.expectedResult
+
+		t.Run(c.name, func(t *testing.T) {
+			points := trimmedDescriptionPoints(items)
+
+			assert.Equal(t, expectedPoints, points)
+		})
+	}
+}
+
+func Test_oddPurchaseDayPoints(t *testing.T) {
+	cases := []struct {
+		name           string
+		date           string
+		expectedResult int
+	}{
+		{
+			name:           "odd-day-case",
+			date:           "2022-01-01",
+			expectedResult: 6,
+		},
+		{
+			name:           "even-day-case",
+			date:           "2022-01-20",
+			expectedResult: 0,
+		},
+	}
+
+	for _, c := range cases {
+		expectedPoints := c.expectedResult
+		date, err := time.Parse(datePurchaseFormat, c.date)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		t.Run(c.name, func(t *testing.T) {
+			points := oddPurchaseDayPoints(date)
+
+			assert.Equal(t, expectedPoints, points)
+		})
+	}
+}
+
+func Test_timePurchasePoints(t *testing.T) {
+	cases := []struct {
+		name           string
+		tdate          string
+		expectedResult int
+	}{
+		{
+			name:           "hour-in-range",
+			tdate:          "14:00",
+			expectedResult: 10,
+		},
+		{
+			name:           "hour-out-range",
+			tdate:          "13:59",
+			expectedResult: 0,
+		},
+	}
+
+	for _, c := range cases {
+		expectedPoints := c.expectedResult
+		ctime, err := time.Parse(timePurchaseFormat, c.tdate)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		t.Run(c.name, func(t *testing.T) {
+			points := timePurchasePoints(ctime)
+
+			assert.Equal(t, expectedPoints, points)
+		})
+	}
+}
+
+func Test_Points(t *testing.T) {
+	cases := []struct {
+		name           string
+		receipt        receipt.Receipt
+		expectedResult *receipt.Points
+		expectedError  error
+	}{
+		{
+			name: "Target-case",
+			receipt: receipt.Receipt{
+				Retailer:     "Target",
+				PurchaseDate: "2022-01-01",
+				PurchaseTime: "13:01",
+				Items: []receipt.Item{
+					{
+						ShortDescription: "Mountain Dew 12PK",
+						Price:            6.49,
+					},
+					{
+						ShortDescription: "Emils Cheese Pizza",
+						Price:            12.25,
+					},
+					{
+						ShortDescription: "Knorr Creamy Chicken",
+						Price:            1.26,
+					},
+					{
+						ShortDescription: "Doritos Nacho Cheese",
+						Price:            3.35,
+					},
+					{
+						ShortDescription: "   Klarbrunn 12-PK 12 FL OZ  ",
+						Price:            12.00,
+					},
+				},
+				Total: 35.35,
+			},
+			expectedResult: &receipt.Points{Points: 28},
+			expectedError:  nil,
+		},
+		{
+			name: "M&M Corner Market-case",
+			receipt: receipt.Receipt{
+				Retailer:     "M&M Corner Market",
+				PurchaseDate: "2022-03-20",
+				PurchaseTime: "14:33",
+				Items: []receipt.Item{
+					{
+						ShortDescription: "Gatorade",
+						Price:            2.25,
+					},
+					{
+						ShortDescription: "Gatorade",
+						Price:            12.25,
+					},
+					{
+						ShortDescription: "Gatorade",
+						Price:            2.25,
+					},
+					{
+						ShortDescription: "Gatorade",
+						Price:            12.25,
+					},
+				},
+				Total: 9.00,
+			},
+			expectedResult: &receipt.Points{Points: 109},
+			expectedError:  nil,
+		},
+		{
+			name: "error-date-case",
+			receipt: receipt.Receipt{
+				Retailer:     "M&M Corner Market",
+				PurchaseDate: "2022-03-32",
+				PurchaseTime: "14:33",
+				Items: []receipt.Item{
+					{
+						ShortDescription: "Gatorade",
+						Price:            2.25,
+					},
+					{
+						ShortDescription: "Gatorade",
+						Price:            12.25,
+					},
+					{
+						ShortDescription: "Gatorade",
+						Price:            2.25,
+					},
+					{
+						ShortDescription: "Gatorade",
+						Price:            12.25,
+					},
+				},
+				Total: 9.00,
+			},
+			expectedResult: nil,
+			expectedError:  errors.New("invalid date format"),
+		},
+		{
+			name: "error-time-case",
+			receipt: receipt.Receipt{
+				Retailer:     "M&M Corner Market",
+				PurchaseDate: "2022-03-20",
+				PurchaseTime: "25:33",
+				Items: []receipt.Item{
+					{
+						ShortDescription: "Gatorade",
+						Price:            2.25,
+					},
+					{
+						ShortDescription: "Gatorade",
+						Price:            12.25,
+					},
+					{
+						ShortDescription: "Gatorade",
+						Price:            2.25,
+					},
+					{
+						ShortDescription: "Gatorade",
+						Price:            12.25,
+					},
+				},
+				Total: 9.00,
+			},
+			expectedResult: nil,
+			expectedError:  errors.New("invalid time format"),
+		},
+	}
+
+	for _, c := range cases {
+		ctx := context.Background()
+		r := c.receipt
+		expectedPoints := c.expectedResult
+		expectedError := c.expectedError
+
+		t.Run(c.name, func(t *testing.T) {
+			points, err := Points(ctx, r)
+
+			assert.Equal(t, expectedPoints, points)
+			assert.Equal(t, expectedError, err)
+		})
+	}
+}
